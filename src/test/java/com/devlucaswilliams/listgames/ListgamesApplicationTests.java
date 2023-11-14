5289df043c070d643c0d0842394014f9988679a9
Lucas williams <lucaswilliamscesar@gmail.com>
2023-11-14 05:47:59 -0300

    Project created

---
+++ src/test/java/com/devlucaswilliams/listgames/ListgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucaswilliams.listgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
